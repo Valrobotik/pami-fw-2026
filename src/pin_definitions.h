504f283d0a097c41c27e3f4c049e3affda65c4e5
Rémi Ait-Younes <remi@ait-younes.fr>
2026-05-05 21:27:58 +0200

    add rev2 pin_definitions.h

--- src/pin_definitions.h
+++ src/pin_definitions.h
@@ -1,19 +1,16 @@
 #pragma once
 
 // HW Rev 01 pin definitions
-#define EN_1_PIN 6
-#define DIR_1_PIN 7
-#define STEP_1_PIN 8
-#define VREF_1_PIN 10
-#define SLEEP_1_PIN 5
-#define FAULT_1 9
+#define EN_1_PIN 10
+#define DIR_1_PIN 8
+#define STEP_1_PIN 9
 
-#define EN_2_PIN 18
-#define DIR_2_PIN 33
-#define STEP_2_PIN 48
-#define VREF_2_PIN 47
-#define SLEEP_2_PIN 17
-#define FAULT_2_PIN 38
+#define EN_2_PIN 33
+#define DIR_2_PIN 48
+#define STEP_2_PIN 47
+
+#define UART_TX_PIN 6
+#define UART_RX_PIN 7
 
 #define SCL_PIN 13
 #define SDA_PIN 14
